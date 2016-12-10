a411baa07b868ebc4f79bb6894a99a028e69111b
Mitch Craig <mitch@mitchellcraig.com>
2016-12-10 15:19:35 -0700

    Improved game loop for movement.

--- examples/scripts/system/game.py
+++ examples/scripts/system/game.py
@@ -1,7 +1,7 @@
 import entityx
-from _entityx_components import Body, Renderable, InputResponder
+from _entityx_components import Body, Renderable, InputResponder, Destroyed
 from gamemath import vector2
-from system.tile import Tile, TileType, Stats
+from system.tile import Tile, TileType, Stats, Upgrade
 import sys
 Vector2 = vector2.Vector2
 
@@ -22,62 +22,90 @@ class Game(entityx.Entity):
                 
         self.nickCage = Tile(TileType.nickCage, 1, 1, Stats(5, 1))
         self.moveableMap[1][1] = self.nickCage
-        self.moveableMap[2][1] = Tile(TileType.chest, 2, 1)
+        self.moveableMap[2][1] = Tile(TileType.chest, 2, 1, upgrade = Upgrade(1, 1))
         self.moveableMap[1][2] = Tile(TileType.clue, 1, 2)
 
     def update(self, dt):
         # Move Nick Cage based on InputResponder
         input_events = self.inputResponder.responds
         if "-MoveUp" in input_events:
+            print "Move Up"
             self.nickCage.body.direction.x = 0
             self.nickCage.body.direction.y = -1
         if "-MoveDown" in input_events:
+            print "Move Down"
             self.nickCage.body.direction.x = 0
             self.nickCage.body.direction.y = 1
         if "-MoveRight" in input_events:
+            print "Move Right"
             self.nickCage.body.direction.x = 1
             self.nickCage.body.direction.y = 0
         if "-MoveLeft" in input_events:
+            print "Move Left"
             self.nickCage.body.direction.x = -1
             self.nickCage.body.direction.y = 0
         
         # Damage loop. If damage is dealt, indicate that the tile cannot move
-        for x in range(0, len(self.moveableMap)-1):
-            for y in range(0, len(self.moveableMap[x])-1):
+        for x in range(0, len(self.moveableMap)):
+            for y in range(0, len(self.moveableMap[x])):
                 tile = self.moveableMap[x][y]
                 if tile != None:
                     # Calculate the new tile position
                     newTileX = tile.gameBody.x + int(tile.body.direction.x)
                     newTileY = tile.gameBody.y + int(tile.body.direction.y)
                     tileInNewPosition = self.moveableMap[newTileX][newTileY]
-                    if tileInNewPosition != None:
+                    if tileInNewPosition != None and tileInNewPosition != tile:
                         if tileInNewPosition.stats != None and tile.stats != None:
                             # Do damage to each other
+                            tileInNewPosition.stats.health -= tile.stats.weapon
+                            tile.stats.health -= tileInNewPosition.stats.weapon
+                            if tile.stats.health <= 0:
+                                tile.destroyed = tile.Component(Destroyed)
+                            if tileInNewPosition.stats.health <= 0:
+                                tileInNewPosition.destroyed = tileInNewPosition.Component(Destroyed)
                             tileInNewPosition.gameBody.canMove = False
                             tile.gameBody.canMove = False
                         else:
-                            if self.staticMap[newTileX][newTileY].tileType != TileType.wall:
+                            print "Collision detected with non-stat entity!"
+                            if tileInNewPosition != None and tileInNewPosition.upgrade != None:
+                                print "Upgrade obtained!"
+                                tile.stats.health += tileInNewPosition.upgrade.health
+                                tile.stats.weapon += tileInNewPosition.upgrade.weapon
+                                tileInNewPosition.destroyed = tileInNewPosition.Component(Destroyed) # Destroy the upgrade (single-use)
+                                tile.gameBody.x = newTileX
+                                tile.gameBody.y = newTileY
+                                tile.gameBody.canMove = True
+                            elif self.staticMap[newTileX][newTileY].tileType != TileType.wall:
                                 tile.gameBody.x = newTileX
                                 tile.gameBody.y = newTileY
                                 tile.gameBody.canMove = True
                             else:
+                                print "Trying to wall hack?"
                                 tile.gameBody.canMove = False
-                    else:
+                    elif tileInNewPosition != tile:
                         tile.gameBody.canMove = True
+                    else:
+                        tile.gameBody.canMove = False
                         
         # Movement loop. If the tile can move, do it.
-        for x in range(0, len(self.moveableMap)-1):
-            for y in range(0, len(self.moveableMap[x])-1):
+        for x in range(0, len(self.moveableMap)):
+            for y in range(0, len(self.moveableMap[x])):
                 tile = self.moveableMap[x][y]
-                if tile != None and tile.gameBody.canMove == True:
-                    self.moveableMap[tile.gameBody.x][tile.gameBody.x] = tile
-                    self.moveableMap[x][y] = None
+                if tile != None:
+                    if tile.gameBody.canMove == True:
+                        # Sync the map to wherever the entity thinks it is
+                        self.moveableMap[tile.gameBody.x][tile.gameBody.x] = tile
+                        self.moveableMap[x][y] = None
+                    else:
+                        # Revert any movement that may have tried to occur
+                        tile.gameBody.x = x
+                        tile.gameBody.y = y
                         
-                    # Sync the physical body based on the new tile position
+                    # Sync the physical body based on the tile position
                     tile.body.position.x = tile.gameBody.x * TILESIZE_X
                     tile.body.position.y = tile.gameBody.y * TILESIZE_Y
     
     def GenerateLevelLayout(self, tileMap):
-        for x in range(0, len(tileMap)-1):
-            for y in range(0, len(tileMap[x])-1):
+        for x in range(0, len(tileMap)):
+            for y in range(0, len(tileMap[x])):
                 self.staticMap[x][y] = Tile(tileMap[x][y], x, y)
--- examples/scripts/system/tile.py
+++ examples/scripts/system/tile.py
@@ -8,15 +8,21 @@ TILESIZE_X = 32
 TILESIZE_Y = 32
 
 class Tile(entityx.Entity):
-    def __init__(self, tileType, tileX, tileY, stats = None):
+    def __init__(self, tileType, tileX, tileY, stats = None, upgrade = None):
         self.body = self.Component(Body)
-        self.body.x = tileX * TILESIZE_X
-        self.body.y = tileY * TILESIZE_Y
+        self.body.position.x = tileX * TILESIZE_X
+        self.body.position.y = tileY * TILESIZE_Y
         self.stats = stats
         self.gameBody = GameBody()
         self.gameBody.x = tileX
         self.gameBody.y = tileY
         self.tileType = tileType
+        self.renderable = self.Component(Renderable)
+        self.renderable.texture = "./images/Tiles.ase"
+        self.renderable.layer = TileType.GetLayer(self.tileType)
+        self.upgrade = upgrade
+        
+        self.renderable.currentAnim = TileType.GetName(self.tileType)
 
     def update(self, dt):
         # Do nothing.
@@ -40,6 +46,15 @@ class GameBody(object):
     def update(self, dt):
         # Do nothing.
         self.updated = True
+
+class Upgrade(object):
+    def __init__(self, health = 0, weapon = 0):
+        self.health = health
+        self.weapon = weapon
+        
+    def update(self, dt):
+        # Do nothing.
+        self.updated = True
         
 class TileType(object):
     wall = 1
@@ -50,3 +65,27 @@ class TileType(object):
     lava = 6
     nickCage = 7
     fbi = 8
+    
+    @classmethod
+    def GetName(self, tileType):
+        return {self.wall : "Wall",
+                self.floor : "Floor",
+                self.door : "Door",
+                self.chest : "Chest",
+                self.clue : "Clue",
+                self.lava : "Lava",
+                self.nickCage : "NC",
+                self.fbi : "FBI"
+                }.get(tileType, self.wall)
+                
+    @classmethod
+    def GetLayer(self, tileType):
+        return {self.wall : 0,
+                self.floor : 0,
+                self.door : 1,
+                self.chest : 1,
+                self.clue : 1,
+                self.lava : 0,
+                self.nickCage : 1,
+                self.fbi : 1
+                }.get(tileType, self.wall)
